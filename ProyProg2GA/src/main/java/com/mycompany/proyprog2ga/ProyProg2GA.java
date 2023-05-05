7a56627137d0d5a192d6e1eaeb24656ddb41ceb1
octavio4452 <66646120+octavio4452@users.noreply.github.com>
2023-05-05 09:34:44 -0500

    Revert "mejora de la clase"
    This reverts commit 97c71dfb3b087066108082b8c887063fb745b9ae.

--- ProyProg2GA/src/main/java/com/mycompany/proyprog2ga/ProyProg2GA.java
+++ ProyProg2GA/src/main/java/com/mycompany/proyprog2ga/ProyProg2GA.java
@@ -1,8 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
 
 package com.mycompany.proyprog2ga;
 
-
+/**
+ *
+ * @author octaviux
+ */
 public class ProyProg2GA {
 
     public static void main(String[] args) {
