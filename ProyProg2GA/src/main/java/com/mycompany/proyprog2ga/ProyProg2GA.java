744de9e0019da0a2ebc2ff532b6e34d777adfa68
octavio4452 <66646120+octavio4452@users.noreply.github.com>
2023-05-05 09:31:14 -0500

    trabajo dia 1
    realizamos la clase viuelo para controala

---
+++ ProyProg2GA/src/main/java/com/mycompany/proyprog2ga/ProyProg2GA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyprog2ga;
+
+/**
+ *
+ * @author octaviux
+ */
+public class ProyProg2GA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
